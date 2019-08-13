918b398b868b56c82e602886bece3336ff9b24d4
Roman <roengele@student.ethz.ch>
2019-08-13 13:36:21 -0700

    helper files

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,5 +1,6 @@
 from collections import deque
 import random
+import numpy as np
 
 class replay_buffer():
     def __init__(self, buffer_size):
@@ -11,17 +12,23 @@ class replay_buffer():
     def sample(self, batch_size):
         ''' sample new batch from replay buffer '''
         # random draw N
-        random.seed()
+        # random.seed()
         return random.sample(self.buffer, batch_size)
 
     def add(self, new_experience):
         ''' add new experience to replay buffer '''
         if self.num_experiences < self.buffer_size:
             self.buffer.append(new_experience)
-            self.num_experiences += 1
+            #self.num_experiences += 1
         else:
-            self.buffer.pop(0)
-            self.buffer.append(new_experience)
+            #pop_idx = (np.random.geometric(0.005)-1)% self.buffer_size
+            keep_prob = 1.* self.buffer_size/self.num_experiences
+            if np.random.rand() < keep_prob:
+                pop_idx = np.random.randint(0, self.buffer_size)
+                self.buffer.pop(pop_idx)
+                self.buffer.append(new_experience)
+
+        self.num_experiences += 1
 
     def reset(self):
         self.buffer = [] #.clear()
