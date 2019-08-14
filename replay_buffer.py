1ec67bf9344b1584901726fe1e29371e8760484e
Roman <roengele@student.ethz.ch>
2019-08-14 11:13:11 -0700

    .

--- replay_buffer.py
+++ replay_buffer.py
@@ -19,9 +19,12 @@ class replay_buffer():
         ''' add new experience to replay buffer '''
         if self.num_experiences < self.buffer_size:
             self.buffer.append(new_experience)
-            #self.num_experiences += 1
+            self.num_experiences += 1
         else:
-            #pop_idx = (np.random.geometric(0.005)-1)% self.buffer_size
+            pop_idx = 0#(np.random.geometric(0.005)-1)% self.buffer_size
+            self.buffer.pop(pop_idx)
+            self.buffer.append(new_experience)
+            '''
             keep_prob = 1.* self.buffer_size/self.num_experiences
             if np.random.rand() < keep_prob:
                 pop_idx = np.random.randint(0, self.buffer_size)
@@ -29,6 +32,7 @@ class replay_buffer():
                 self.buffer.append(new_experience)
 
         self.num_experiences += 1
+        '''
 
     def reset(self):
         self.buffer = [] #.clear()
