3eb47de2a52d8cc7b73989bc037ae4b61fe3bee6
Roman Engeler <roengele@student.ethz.ch>
2019-09-10 17:47:34 -0700

    .

---
+++ RL2/environment.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+class environment():
+    ''' Grid wold consisting of N squares with reward sampled at random in corners '''
+
+    def __init__(self, state_space):
+        # size of square
+        self.size = state_space
+
+        # initialize agent
+        self.state = np.zeros([self.size])
+        self.state[(self.size-1)/2] = 1
+
+        # termination flag
+        self.d = 0
+
+        # initialize target
+        self.target = np.array((self.size-1) * np.random.randint(0, 2))
+        #self.target = self.size - 1
+
+        self.rew_mag = 1. + np.random.normal()
+
+    def _sample_env(self):
+        ''' resample delta '''
+        self.target = np.array((self.size-1) * np.random.randint(0, 2))
+        #self.target = self.size- 1
+        self.rew_mag = 1.+np.random.normal()
+
+    def _sample_state(self):
+        self.state = np.zeros([self.size])
+        self.state[(self.size - 1) / 2] = 1
+        return self.state
+
+    def reward(self):
+        ''' reward function '''
+
+        '''
+        p = 1.*(self.size- 1- np.abs(self.target- np.argmax(self.state)))/ (self.size- 1)
+        if p > np.random.rand():
+            rew = 1.
+        else:
+            rew = 0.
+        '''
+
+        return 1.*(self.size- 1- np.abs(self.target- np.argmax(self.state)))/ (self.size- 1)
+
+    def termination(self):
+        ''' determine termination of MDP '''
+        return self.d
+
+    def _step(self, action):
+        '''
+        interact with environment and return observation [s', r, d]
+        '''
+
+        # update position
+        pos = np.where(self.state == 1)[0]
+
+        # update termination flag
+        d = False
+
+        if not d:
+            if action == 1 and pos != 0: # left
+                self.state[pos] = 0
+                self.state[pos- 1] = 1
+            if action == 2 and pos != self.size-1: # down
+                self.state[pos] = 0
+                self.state[pos+ 1] = 1
+
+        # update reward
+        r = self.reward()
+
+        # stack observation
+        obs = np.array([self.state, r, d])
+
+        return obs
