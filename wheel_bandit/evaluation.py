214da406310761daba362edeb6e9ec2b17cc08c1
Roman <roengele@student.ethz.ch>
2019-09-18 23:30:05 -0700

    evaluation with tensorflow

--- wheel_bandit/evaluation.py
+++ wheel_bandit/evaluation.py
@@ -13,28 +13,20 @@ tf.flags.DEFINE_integer("batch_size", 2, "Batch size for training")
 tf.flags.DEFINE_integer("action_space", 5, "Dimensionality of action space")
 tf.flags.DEFINE_integer("state_space", 2, "Dimensionality of state space")
 tf.flags.DEFINE_integer("hidden_space", 128, "Dimensionality of hidden space")
-tf.flags.DEFINE_integer("latent_space", 64, "Dimensionality of latent space")
+tf.flags.DEFINE_integer("latent_space", 16, "Dimensionality of latent space")
 tf.flags.DEFINE_float("gamma", 0., "Discount factor")
 
 tf.flags.DEFINE_float("learning_rate", 5e-3, "Initial learning rate")
-tf.flags.DEFINE_float("lr_drop", 1.00015, "Drop of learning rate per episode")
+tf.flags.DEFINE_float("regularizer", 1.0, "Regularization parameter")
 
 tf.flags.DEFINE_float("prior_precision", 0.5, "Prior precision (1/var)")
 tf.flags.DEFINE_float("noise_precision", 0.01, "Noise precision (1/var)")
-tf.flags.DEFINE_float("noise_precmax", 20, "Maximum noise precision (1/var)")
-tf.flags.DEFINE_integer("noise_Ndrop", 30, "Increase noise precision every N steps")
-tf.flags.DEFINE_float("noise_precstep", 1.001, "Step of noise precision s*=ds")
 
 tf.flags.DEFINE_integer("L_episode", 500, "Length of episodes")
-
-tf.flags.DEFINE_integer("split_N", 30, "Increase split ratio every N steps")
-tf.flags.DEFINE_float("split_ratio", 0.9, "Initial split ratio for conditioning")
+tf.flags.DEFINE_float("split_ratio", 0.35, "Initial split ratio for conditioning")
 tf.flags.DEFINE_integer("update_freq_post", 1, "Update frequency of posterior and sampling of new policy")
 
-tf.flags.DEFINE_integer("replay_memory_size", 1000, "Size of replay memory")
 tf.flags.DEFINE_integer("iter_amax", 1, "Number of iterations performed to determine amax")
-tf.flags.DEFINE_integer("save_frequency", 400, "Store images every N-th episode")
-tf.flags.DEFINE_float("regularizer", 1.0, "Regularization parameter")
 tf.flags.DEFINE_string('non_linearity', 'relu', 'Non-linearity used in encoder')
 
 tf.flags.DEFINE_integer("random_seed", 1234, "Random seed for numpy and tensorflow")
@@ -44,9 +36,6 @@ tf.flags.DEFINE_string('checkpoint', 'latest', 'Name of checkpoint to restore')
 
 FLAGS = tf.flags.FLAGS
 FLAGS(sys.argv)
-
-from wheel_bandit_environment import wheel_bandit_environment
-env = wheel_bandit_environment(FLAGS.action_space, FLAGS.random_seed)
 
 # load tf model
 load_dir = './model/'+ FLAGS.model_name+ '/'
@@ -73,8 +62,8 @@ with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
     QNet = QNetwork(FLAGS)
     Qeval = Qeval(FLAGS)
 
+    # restore model
     saver = tf.train.Saver(max_to_keep=4)
-    #saver.restore(sess, tf.train.latest_checkpoint(load_dir))
     try:
         saver.restore(sess, load_dir + FLAGS.checkpoint)
         print('Successfully restored model from ' + FLAGS.checkpoint)
@@ -85,13 +74,14 @@ with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
     # Loop over deltas (exploration parameter)
     for delta in [0.5, 0.7, 0.9, 0.95, 0.99]:
 
+        # sample data set for specific delta
         # np.hstack((contexts, rewards)), (opt_rewards, opt_actions)
         dataset, opt_wheel = sample_wheel_bandit_data(num_contexts, delta, mean_v, std_v, mu_large, std_large)
 
         # monitor time
         start = time.time()
 
-        # obtain encoding
+        # obtain encoding (for efficiency)
         phi = np.zeros([num_contexts, FLAGS.latent_space, FLAGS.action_space])
         for i in range(0, num_contexts, batch_size):
             phi[i:i+ batch_size] = sess.run(QNet.phi, feed_dict={QNet.state: dataset[i: i+batch_size, :2]})
@@ -107,11 +97,9 @@ with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
         # array to keep track of indices of shuffled array
         data_idx = np.arange(num_contexts)
 
-        # 50 random ordered datasets
+        # 50 random ordered datasets ------------------------
         for shuffle in range(num_datasets):
-            #if shuffle% 10 == 0:
-                #print('Iteration {:2d}...'.format(shuffle))
-            # shuffle dataset
+            # shuffle index
             np.random.shuffle(data_idx)
 
             # array
@@ -123,47 +111,26 @@ with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
             sess.run(QNet.reset_post)
             sess.run(Qeval.sample_prior)
 
-            # loop over dataset
-            for i in range(np.int(FLAGS.split_ratio* FLAGS.L_episode)):
-
-                # calculate posterior
-                try:
-                    sess.run(Qeval.sample_post,
-                                feed_dict={Qeval.context_phi: phi[data_idx[:i]],
-                                        Qeval.context_action: actions[:i],
-                                        Qeval.context_reward: rew_agent_online[:i],
-                                        Qeval.nprec: FLAGS.noise_precision,
-                                        Qeval.is_online: True, Qeval.use_cholesky: False})
-                except:
-                    lttest = sess.run(Qeval.Lt_inv,
-                            feed_dict={Qeval.context_phi: phi[data_idx[:i]],
-                                        Qeval.context_action: actions[:i],
-                                        Qeval.context_reward: rew_agent_online[:i],
-                                        Qeval.nprec: FLAGS.noise_precision,
-                                        Qeval.is_online: True})
-                    print(lttest.shape)
-                    for l in range(len(lttest)):
-                        print(lttest[l])
-                    with open(save_dir + 'cholesky', 'a') as f:
-                        for line in lttest:
-                            np.savetxt(f, line, fmt='%.2f')
-
-                    sess.run(Qeval.sample_post,
-                             feed_dict={Qeval.context_phi: phi[data_idx[:i]],
-                                        Qeval.context_action: actions[:i],
-                                        Qeval.context_reward: rew_agent_online[:i],
-                                        Qeval.nprec: FLAGS.noise_precision,
-                                        Qeval.is_online: True})
-
+            # loop over dataset (context part)
+            for i in range(10000):
                 # prediction
                 Qval = sess.run(Qeval.Qout, feed_dict={Qeval.phi: phi[data_idx[i]].reshape(-1, FLAGS.latent_space, FLAGS.action_space)})
                 action = np.argmax(Qval, axis=1)
 
                 actions[i] = action
-                rew_agent_online[i] = dataset[data_idx[i], 2+ action]
-                rew_rand_online[i] = dataset[data_idx[i], 2 + np.random.randint(0,5)]
+                rew_agent_online[i] = dataset[data_idx[i], 2 + action]
+                rew_rand_online[i] = dataset[data_idx[i], 2 + np.random.randint(0, 5)]
 
-            for i in range(np.int(FLAGS.split_ratio* FLAGS.L_episode), num_contexts, batch_size):
+                # calculate posterior via online updates -> only feed new data point
+                sess.run(Qeval.sample_post,
+                        feed_dict={Qeval.context_phi: phi[data_idx[i]].reshape(-1, FLAGS.latent_space, FLAGS.action_space),
+                                    Qeval.context_action: actions[i:i+1],
+                                    Qeval.context_reward: rew_agent_online[i:i+1],
+                                    Qeval.nprec: FLAGS.noise_precision,
+                                    Qeval.is_online: True, Qeval.use_cholesky: False})
+
+            # after context
+            for i in range(10000, num_contexts, batch_size):
                 # prediction
                 Qval = sess.run(Qeval.Qout, feed_dict={Qeval.phi: phi[data_idx[i:i+batch_size]].reshape(-1, FLAGS.latent_space, FLAGS.action_space)})
                 action = np.argmax(Qval, axis=1)
